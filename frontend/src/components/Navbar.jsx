56e24e4972ec669d01b376e8eca40241aa263476
giteshpawar <giteshpawar3516@gmail.com>
2026-09-24 18:35:59 +0530

    Navbar name changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -375,7 +375,7 @@ export default function Navbar({
         <a className="drawer-link" onClick={goHome}>Home</a>
         <a className="drawer-link" onClick={scrollToNewsroom}>Newsroom</a>
         <a className="drawer-link" onClick={(e) => handleNavClick(e, "/rss", onRss)}>RSS Feed</a>
-        <a className="drawer-link" onClick={(e) => handleNavClick(e, "/how", onAbout)}>About the desk</a>
+        <a className="drawer-link" onClick={(e) => handleNavClick(e, "/how", onAbout)}>About the Cyberbriefs</a>
         <a className="drawer-link" onClick={(e) => handleNavClick(e, "/blogs", onBlogs)}>Blogs</a>
         <a className="drawer-link" onClick={(e) => handleNavClick(e, "/books", onBooks)}>Books</a>
         <a className="drawer-link" onClick={(e) => handleNavClick(e, "/join", null)}>Careers</a>
@@ -400,7 +400,7 @@ export default function Navbar({
         </div>
  
         <div className="aggregate-top-right">
-          <a className="aggregate-top-link" href="/how" onClick={(e) => handleNavClick(e, "/how", onAbout)}>About the desk</a>
+          <a className="aggregate-top-link" href="/how" onClick={(e) => handleNavClick(e, "/how", onAbout)}>About the Cyberbrief</a>
           <a className="aggregate-top-link" href="/blogs" onClick={(e) => handleNavClick(e, "/blogs", onBlogs)}>Blogs</a>
           <a className="aggregate-top-link" href="/books" onClick={(e) => handleNavClick(e, "/books", onBooks)}>Books</a>
           <button className="aggregate-subscribe" type="button" onClick={(e) => { e.preventDefault(); if (onSubscribe) onSubscribe(); }}>Subscribe</button>
